43096db23ff0675b616a40e67538ca49d5c9fbf3
Enzo Sanchez Valero <sanchezvaleroe@educ.college-montreal.qc.ca>
2024-02-06 16:17:20 -0500

    Initial commit

---
+++ game_state.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class GameState(Enum):
+    NOT_STARTED = 0
+    ROUND_DONE = 0
+    GAME_OVER = 0
+    ROUND_ACTIVE = 0
+    def __init__(self):
+        super.__init__()
+
+    def on_key_press:
+        pass
+
+
